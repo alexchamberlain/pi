70cff0b405137fe89f837b9377f5467ac8006eb7
Alex Chamberlain <alex@alexchamberlain.co.uk>
2012-09-17 08:27:56 +0100

    Initial commit of libpi (Linux)

---
+++ lib/linux/dd.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int dd(int in_fd, int out_fd, size_t buffersize) {
+  char * buffer;
+  size_t pagesize = sysconf(_SC_PAGESIZE);
+  size_t count;
+
+  /* Memory should be page-aligned to avoid issues with funny drivers. */
+  if(posix_memalign((void**) &buffer, pagesize, buffersize) != 0 || buffer == NULL) {
+    fprintf(stderr, "Failed to allocate buffer.\n");
+    return 1;
+  }
+
+  memset(buffer, 0, buffersize);
+  while((count = read(in_fd, buffer, buffersize)) != 0) {
+    if(write(out_fd, buffer, count) < count) {
+      fprintf(stderr, "Failed to write the buffer to output.\n");
+      goto error;
+    }
+    memset(buffer, 0, buffersize);
+  }
+
+  if(count == -1) {
+    fprintf(stderr, "Failed to write the buffer to output.\n");
+    goto error;
+  }
+
+  free(buffer);
+
+  return 0;
+
+  error:
+    if(buffer != NULL) {
+      /* Clear buffer to avoid any security problems */
+      memset(buffer, 0, buffersize);
+      free(buffer);
+    }
+    return 1;
+}
---
+++ lib/linux/pi.h
@@ -0,0 +1,6 @@
+#ifndef PI_H
+#define PI_H
+
+int dd(int in_fd, int out_fd, size_t buffersize);
+
+#endif /* PI_H */
